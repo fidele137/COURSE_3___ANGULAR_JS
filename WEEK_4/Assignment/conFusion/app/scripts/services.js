5b2c71c367f3f7b636aa478464d5c590e28ac577
Fidele <fidele.palouki@gmail.com>
2016-12-20 22:28:54 +0100

    folders included

---
+++ WEEK_4/Assignment/conFusion/app/scripts/services.js
@@ -0,0 +1,36 @@
+'use strict';
+
+angular.module('confusionApp')
+
+        .constant("baseURL","http://localhost:3000/")
+
+        .service('menuFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+    
+    
+                this.getDishes = function(){  
+                    return $resource(baseURL+"dishes/:id", null,  {'update':{method:'PUT' }});
+                };
+    
+    
+                // implement a function named getPromotion
+                // that returns a selected promotion.
+                this.getPromotion = function(){
+                    return $resource(baseURL+"promotions/:id", null,  {'update':{method:'PUT' }});
+                };
+                     
+        }])
+
+
+        .factory('corporateFactory', ['$resource', 'baseURL', function($resource, baseURL) {
+  
+            return $resource(baseURL+"leadership/:id", null,  {'update':{method:'PUT' }});
+    
+        }])
+
+
+        .factory('feedbackFactory', ['$resource', 'baseURL', function($resource,baseURL) {
+    
+                return $resource(baseURL+"feedback/:id", null, {'save':{method:'POST' }});
+        }])
+
+;
